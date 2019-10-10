2632c13b37ae70879d392fe3dd6ea95759428331
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-10 21:52:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,7 @@ def find_the_cheese(contains_cheddar)
       contains_cheddar.each do |element|
       if element[0] == "cheddar" || element[0] == "gouda" || element[0] == "gouda"
         return element[0]
-      else
-        return nil
+      
       end
     end
     
